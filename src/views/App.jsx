553c347770a9059e5b0a50af1d1694c5d6db41a1
MoneyIgos <makowskiigor@gmail.com>
2021-04-14 09:24:01 +0200

    feat(views): create styles for Wrapper

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,9 +1,20 @@
 import UsersList from 'components/UsersList/UsersList'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  background-color: #202020;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  color: #fff;
+`
 
 const App = () => (
-  <>
+  <Wrapper>
     <UsersList />
-  </>
+  </Wrapper>
 )
 
 export default App
