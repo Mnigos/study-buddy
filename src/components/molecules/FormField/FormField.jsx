6ea9de5b6c0b9678b9ce41b3ecee70de10c81df9
MoneyIgos <makowskiigor@gmail.com>
2021-04-14 14:34:03 +0200

    feat(components): add FormField

---
+++ src/components/molecules/FormField/FormField.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import { Label } from 'components/atoms/Label/Label'
+import { Input } from 'components/atoms/Input/Input'
+import { Wrapper } from './FormField.style'
+
+const FormField = ({ label, name, id, type = 'text', ...props }) => {
+  return (
+    <Wrapper>
+      <Label htmlFor={id}>{label}</Label>
+      <Input name={name} id={id} type={type} />
+    </Wrapper>
+  )
+}
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string,
+}
+
+export default FormField
