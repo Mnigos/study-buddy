16c37c1d737099c59bb5a68ea0e3a489360b46e7
MoneyIgos <makowskiigor@gmail.com>
2021-04-15 14:35:55 +0200

    fix(renderWithProvider): change function name

--- src/helpers/renderWithProvider.jsx
+++ src/helpers/renderWithProvider.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import { theme } from 'assets/styles/theme'
 import UsersProvider from 'providers/UsersProvider'
 
-export const renderWithThemeProvider = children => {
+export const renderWithProvider = children => {
   return render(
     <ThemeProvider theme={theme}>
       <UsersProvider>{children}</UsersProvider>
